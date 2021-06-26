3861ada71c89701c6b03f6da673d1b2621c6fc5c
Adibazz <adib.badjerei@outlook.com>
2021-06-26 14:36:05 +0700

    update validator

--- src/validator/musics/schema.js
+++ src/validator/musics/schema.js
@@ -1,11 +1,14 @@
+/* eslint-disable newline-per-chained-call */
 const Joi = require('joi');
+
+const thisYear = new Date().getFullYear();
 
 const MusicPayloadSchema = Joi.object({
   title: Joi.string().required(),
-  year: Joi.number().required(),
+  year: Joi.number().integer().min(1970).max(thisYear).required(),
   performer: Joi.string().required(),
   genre: Joi.string().required(),
-  duration: Joi.number(),
+  duration: Joi.number().integer().greater(0),
 });
 
 module.exports = { MusicPayloadSchema };
