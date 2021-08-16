bb3162bf557383fe634e13823e170773f1afc479
Azzadib <adib.badjerei@outlook.com>
2021-08-17 04:49:26 +0700

    create upload image

--- src/server.js
+++ src/server.js
@@ -3,6 +3,8 @@ require('dotenv').config();
 //* const ClientError = require('./exceptions/ClientError');
 const Hapi = require('@hapi/hapi');
 const Jwt = require('@hapi/jwt');
+const path = require('path');
+
 const musics = require('./api/musics');
 const MusicsService = require('./services/postgres/MusicsService');
 const MusicsValidator = require('./validator/musics');
@@ -32,6 +34,10 @@ const _exports = require('./api/exports');
 const ProducerService = require('./services/rabbitmq/ProducerService');
 const ExportsValidator = require('./validator/exports');
 
+const uploads = require('./api/uploads');
+const StorageService = require('./services/storage/StorageService');
+const UploadsValidator = require('./validator/uploads');
+
 const init = async () => {
   const collaborationsService = new CollaborationsService();
   const musicsService = new MusicsService();
@@ -39,6 +45,7 @@ const init = async () => {
   const authenticationsService = new AuthenticationsService();
   const playlistsService = new PlaylistsService(collaborationsService);
   const playlistSongsService = new PlaylistsSongsService(collaborationsService);
+  const storageService = new StorageService(path.resolve(__dirname, 'api/uploads/file/pictures'));
 
   const server = Hapi.server({
     port: process.env.PORT,
@@ -122,6 +129,13 @@ const init = async () => {
         service: ProducerService,
         validator: ExportsValidator,
         playlistsService,
+      },
+    },
+    {
+      plugin: uploads,
+      options: {
+        service: storageService,
+        validator: UploadsValidator,
       },
     },
   ]);
